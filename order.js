fa38c736fe9e5e14724fc7cf021a5aec7d869238
Maas Lalani <maaslalani1@gmail.com>
2019-02-21 14:54:08 -0500

    Add message to confirmation

--- order.js
+++ order.js
@@ -6,4 +6,4 @@ document.getElementById('name').innerHTML = `${localStorage.getItem('name')}, `
 document.getElementById('price').innerHTML = `$${parseFloat(localStorage.getItem('price')).toFixed(2)}`
 document.getElementById('recipient').innerHTML = localStorage.getItem('recipient')
 document.getElementById('address').innerHTML = `${localStorage.getItem('mailing')}, ${localStorage.getItem('location')}, ${localStorage.getItem('postal')}`
-document.getElementById('text').innerHTML = localStorage.getItem('text')
+document.getElementById('text').innerHTML = localStorage.getItem('message')
