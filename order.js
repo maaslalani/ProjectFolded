22b0bf957f771fb7c3bfe36c87ef692fe2a64050
Maas Lalani <maaslalani1@gmail.com>
2019-02-20 20:41:11 -0500

    Add payment screen

---
+++ order.js
@@ -0,0 +1,9 @@
+let paymentButton = document.getElementById('payment-button')
+
+paymentButton.href = `https://www.paypal.me/projecfolded/${localStorage.getItem('price')}`
+
+document.getElementById('name').innerHTML = `${localStorage.getItem('name')}, `
+document.getElementById('price').innerHTML = `$${parseFloat(localStorage.getItem('price')).toFixed(2)}`
+document.getElementById('recipient').innerHTML = localStorage.getItem('recipient')
+document.getElementById('address').innerHTML = `${localStorage.getItem('mailing')}, ${localStorage.getItem('location')}, ${localStorage.getItem('postal')}`
+document.getElementById('text').innerHTML = localStorage.getItem('text')
--- script.js
+++ script.js
@@ -8,16 +8,27 @@ function populateForm() {
   continueForm.style.display = 'block'
 }
 
-function submitForm() {
+function submitForm(e) {
   let message = document.getElementById('message').value
   let mailing = [...document.getElementsByTagName('input')].map(input => input.value)
   let origami = selectedType.querySelector('h4').textContent
+  let price = BASE_PRICE + parseInt(selectedType.querySelector('h5').textContent.substring(2))
 
-  let price = BASE_PRICE + selectedType.querySelector('h5').textContent
-  console.log(selectedType)
-  console.log(message)
-  console.log(mailing)
-  console.log(price)
+  let unfilled = false
+  mailing.forEach(element => {if (!element) { unfilled = true }})
+  if (unfilled) {
+    alert('There are some blanks left in your form. Please fill it out fully.')
+    e.preventDefault()
+  }
+
+  localStorage.setItem('message', message)
+  localStorage.setItem('name', mailing[0])
+  localStorage.setItem('recipient', mailing[1])
+  localStorage.setItem('mailing', mailing[2])
+  localStorage.setItem('postal', mailing[3])
+  localStorage.setItem('location', mailing[4])
+  localStorage.setItem('origami', origami)
+  localStorage.setItem('price', price)
 }
 
 continueForm.style.display = 'none'
